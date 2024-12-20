2304fa892d789cce11f910d56a2a9b08778635bc
Mahmoudx13 <107667374+Mahmoudx13@users.noreply.github.com>
2024-12-20 12:41:19 -0700

    Add files via upload

---
+++ intro to js/script.js
@@ -0,0 +1,25 @@
+//promot
+var type = prompt("What drink you like ");
+var size = prompt("wjat size droml you want?");
+var sentence =
+  "hello, here is youre" + size + " " + type + ".Have a good day !";
+alert(sentence);
+
+// IPO stands for input, proccess, output. Describes the algrotjhim for wrting simple programs /
+
+// Algritimg steps to solve a programing probplem.
+// input How and and whee you get data (e.g promt for data or create your own data in varibles)
+// proccecing: is what happens to the data.
+/// output: is what you do with the proceesd data (e.g alert, conce log or update oour web)
+
+//e.g. take the drink order progaamr and sepreate its IPO setps.
+//input
+var type = prompt("What drink you like ");
+var size = prompt("what size droml you want?");
+
+//prossesing
+var sentence =
+  "hello, here is you're" + size + " " + type + ".Have a good day !";
+
+//output
+alert(sentence);
